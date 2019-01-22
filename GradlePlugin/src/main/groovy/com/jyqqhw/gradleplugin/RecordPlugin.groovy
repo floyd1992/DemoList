436f26eebadd067d62ff4c73e5c5008dab40dfac
floyd <wangj041@gmail.com>
2019-01-22 23:08:51 +0800

    test1

--- GradlePlugin/src/main/groovy/com/jyqqhw/gradleplugin/RecordPlugin.groovy
+++ GradlePlugin/src/main/groovy/com/jyqqhw/gradleplugin/RecordPlugin.groovy
@@ -1,12 +1,19 @@
 package com.jyqqhw.gradleplugin
 
 import org.gradle.api.Plugin
-import org.gradle.api.Project;
+import org.gradle.api.Project
+import org.gradle.api.tasks.Copy;
 
 class RecordPlugin implements Plugin<Project>{
 
     @Override
     void apply(Project project) {
         println("hahaha, i am a plugin")
+        project.task("checkPostCommitFile"){
+            type:Copy
+            from "$rootDir\\post-commit"
+            into "$rootDir\\.git\\hooks"
+        }
     }
+
 }
